5198df8600241a96e3a73e19582e172b76a0592d
Jefferson Barroso Freitas <jeferssonbarroso@hotmail.com>
2023-12-05 15:56:39 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity//convertpro modelo relacional (BD)
@@ -23,6 +26,11 @@ public class User implements Serializable{
 	private String phone;
 	private String password; 
 	
+	//associação
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
+	
 	public User() {
 		
 	}
@@ -34,6 +42,7 @@ public class User implements Serializable{
 		this.phone = phone;
 		this.password = password;
 	}
+
 
 	public long getId() {
 		return id;
@@ -90,6 +99,10 @@ public class User implements Serializable{
 			return false;
 		User other = (User) obj;
 		return id == other.id;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 	
 	
