9c7b2f30ad7036a0c91eac494f7ef174a5276ca0
Jefferson Barroso Freitas <jeferssonbarroso@hotmail.com>
2023-12-06 17:33:42 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/resources/ProductResource.java
+++ src/main/java/com/educandoweb/course/resources/ProductResource.java
@@ -9,32 +9,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.educandoweb.course.entities.Category;
-import com.educandoweb.course.services.CategoryService;
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.services.ProductService;
 
 @RestController 
 //notação que significa que essa classe é um recurso web que é implementado por um
 //controlador REST
-@RequestMapping(value = "/categories")
+@RequestMapping(value = "/products")
 //dá o nome ao recurso que se deseja chamar
+
 public class ProductResource {
 	
 	@Autowired
-	private CategoryService service; //injeção de dep.
+	private ProductService service; //injeção de dep.
 	
 	@GetMapping 
 	// indidca que método responde a uma inquisição GET do HTTP
 	//metodo pra acessar usuarios
 	
 
-	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = service.findaAll();
+	public ResponseEntity<List<Product>> findAll(){
+		List<Product> list = service.findaAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Category> findById(@PathVariable Long id){
-		Category obj = service.findById(id);
+	public ResponseEntity<Product> findById(@PathVariable Long id){
+		Product obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 	
